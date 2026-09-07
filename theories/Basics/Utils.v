504c47293258cd3efcc8be2d47e2c2956488853e
Roger Burtonpatel <burtonpatel1@gmail.com>
2026-09-06 21:47:49 -0400

    further simplify defs

--- theories/Basics/Utils.v
+++ theories/Basics/Utils.v
@@ -11,31 +11,12 @@ Lemma hexploit_mp: forall P Q: Type, P -> (P -> Q) -> Q.
 Proof. intuition. Defined.
 Ltac hexploit x := eapply hexploit_mp; [eapply x|].
 
-Ltac rewrite_everywhere lem :=
-  progress ((repeat match goal with [H: _ |- _] => rewrite lem in H end); repeat rewrite lem).
-
 Ltac rewrite_everywhere_except lem X :=
   progress ((repeat match goal with [H: _ |- _] =>
                  match H with X => fail 1 | _ => rewrite lem in H end
              end); repeat rewrite lem).
 
 Global Tactic Notation "intros !" := repeat intro.
-
-Ltac flatten_goal :=
-  match goal with
-  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
-  end.
-
-Ltac flatten_hyp h :=
-  match type of h with
-  | context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
-  end.
-
-Ltac flatten_all :=
-  match goal with
-  | h: context[match ?x with | _ => _ end] |- _ => let Heq := fresh "Heq" in destruct x eqn:Heq
-  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
-  end.
 
 (* inv by name of the Inductive relation *)
 Ltac invn f :=
@@ -63,20 +44,6 @@ Ltac destructn f :=
     | [ id: f _ _ _ _ _ _ |- _ ] => destruct id
     | [ id: f _ _ _ _ _ _ _ |- _ ] => destruct id
     | [ id: f _ _ _ _ _ _ _ _ |- _ ] => destruct id
-    end.
-
-(* apply by name of the Inductive relation *)
-Ltac appn f :=
-    match goal with
-    | [ id: f |- _ ] => apply id
-    | [ id: f _ |- _ ] => apply id
-    | [ id: f _ _ |- _ ] => apply id
-    | [ id: f _ _ _ |- _ ] => apply id
-    | [ id: f _ _ _ _ |- _ ] => apply id
-    | [ id: f _ _ _ _ _ |- _ ] => apply id
-    | [ id: f _ _ _ _ _ _ |- _ ] => apply id
-    | [ id: f _ _ _ _ _ _ _ |- _ ] => apply id
-    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => apply id
     end.
 
 Ltac break H :=
